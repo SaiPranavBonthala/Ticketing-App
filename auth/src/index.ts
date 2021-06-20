ccbd79fb854dc27416ffaf31d267354587821d24
Bonthala <sai.pranav.bonthala@accenture.com>
2021-06-20 20:40:16 +0530

    Added MongoDB connection setup, Pods and service deployment for Mongodb, Signup Route setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +25,21 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to Mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log(`Listening on Port 3000`);
 });
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -21,10 +22,19 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("Email is already in use");
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
