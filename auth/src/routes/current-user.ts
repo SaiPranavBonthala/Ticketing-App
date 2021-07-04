a6da7e0eac631adc0589c2507ec2ff06f4105721
Bonthala <sai.pranav.bonthala@accenture.com>
2021-07-04 18:54:38 +0530

    Added Signin, Signout, Json web tokens, cookie-sessions middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { currentUser } from "../middleware/current-user";
+
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi There!");
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
