3e18cf6f774c6659b1cba1862034405fd15bbfbb
Bonthala <sai.pranav.bonthala@accenture.com>
2021-07-24 15:23:38 +0530

    Refactoring the redundant code by building a NPM package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { currentUser } from "../middleware/current-user";
+import { currentUser } from "@bspticketsapp/common";
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
+import { validateRequest, BadRequestError } from "@bspticketsapp/common";
 
-import { validateRequest } from "../middleware/validate-request";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
