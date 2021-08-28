f3a369eac887eabf5bac59e6b7af280acfe35ffa
Bonthala <sai.pranav.bonthala@accenture.com>
2021-08-28 16:38:41 +0530

    Updated Client side React pages for front end

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,41 @@
-import buildClient from "../api/build-client";
+import Link from "next/link";
 
-const index = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are not signed in</h1>
+const index = ({ currentUser, tickets }) => {
+  const ticketsList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketsList}</tbody>
+      </table>
+    </div>
   );
 };
 
-index.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-  return data;
+index.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default index;
---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,56 @@
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
+
+const orderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push("/orders"),
+  });
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to Pay: {timeLeft} Seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51JRbjGSImGOeEJTrUURjl7KtRpkjaUreV9tbHvOAniLKls79MDRDzqnLtzGSmtxNCKQ87FtUz161W34WZPUky0fZ00CmZ4zSYJ"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
+};
+
+orderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default orderShow;
