1881857bae3eef560cca8004a5559542fea37e93
Bonthala <sai.pranav.bonthala@accenture.com>
2021-08-29 16:46:40 +0530

    new startup messages

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,8 @@ import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("Starting ...");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
